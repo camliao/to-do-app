4e601d9e0c196cc384743f6313c751af88a68c6e
Cam <camkliao@gmail.com>
2019-05-30 21:21:41 -0400

    added delete button functionality

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
-  // Not sure if adding the deleteItem functionality within the function onReady is correct, as I've done below...
-  // or am I'm supposed to create a new function i.e. `function Delete()` that comes after this function.
-  // When I tried that however, it pretty much "canceled out" all of the code below, as it didn't let me
-  // create a new To-Do-List item; it just caused the page to keep reloading.
-  const deleteItem = document.getElementbyId('deleteItem');
 
   //add event listener to addToDoForm
   addToDoForm.addEventListener('submit', () => {
@@ -24,11 +19,24 @@ function onReady() {
     // set the input's type to checkbox
     checkbox.type = "checkbox";
 
+    // create a delete button
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener('click', function(event){
+      //console.log(event);
+      //this.parentElement
+      toDoList.removeChild(this.parentElement);
+    })
+
     // set the title
     newLi.textContent = title;
 
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
+
+    // attach delete option to li
+    newLi.appendChild(deleteBtn);
 
     // attach the li to the ul
     toDoList.appendChild(newLi);
@@ -38,8 +46,6 @@ function onReady() {
 
   });
  }
-
-
 
  window.onload = function() {
   onReady();
