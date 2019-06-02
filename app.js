0c6f71f51734c0841bacf5cebe0a7f0e41db572b
Cam <camkliao@gmail.com>
2019-06-02 11:21:55 -0400

    added delete button

--- app.js
+++ app.js
@@ -1,36 +1,52 @@
 function onReady() {
   const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
+  let id = 0;
 
   function createNewToDo() {
-    const newToDoText = document.getElementById('newToDoText');
-    if (!newToDoText.value) { return; }
-
-    toDos.push({
+      const newToDoText = document.getElementById('newToDoText');
+      if (!newToDoText.value) { return; }
+      toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id
     });
-    newToDoText.value = '';
 
-    renderTheUI();
+      id++;
+      newToDoText.value = '';
+      renderTheUI();
   }
 
   function renderTheUI() {
-     const toDoList = document.getElementById('toDoList');
+      const toDoList = document.getElementById('toDoList');
+      toDoList.textContent = '';
 
-     toDoList.textContent = '';
+      toDos.forEach(function(toDo) {
+        const newLi = document.createElement('li');
+        const checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
 
-     toDos.forEach(function(toDo) {
-       const newLi = document.createElement('li');
-       const checkbox = document.createElement('input');
-       checkbox.type = "checkbox";
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = "Delete!";
 
-       newLi.textContent = toDo.title;
+        //event listener
+        deleteBtn.addEventListener('click', event => {
+          toDos = toDos.filter(function(item) {
+            return item.id !== toDo.id;
+          })
 
-       toDoList.appendChild(newLi);
-       newLi.appendChild(checkbox);
+          renderTheUI();
+        });
+
+        newLi.textContent = toDo.title;
+
+        toDoList.appendChild(newLi);
+        newLi.appendChild(checkbox);
+        newLi.appendChild(deleteBtn);
     });
+
   }
+
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
@@ -40,7 +56,6 @@ function onReady() {
   renderTheUI();
 }
 
-
- window.onload = function() {
+window.onload = function() {
   onReady();
 };
